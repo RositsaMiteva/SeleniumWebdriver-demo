840e86918bb73705d97317fb9b6ef942ec74eff7
RositsaMiteva <113831422+RositsaMiteva@users.noreply.github.com>
2023-03-12 15:02:27 +0100

    Add project files.

---
+++ SeleniumWebDriverNunitDemo/WebDriverTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SeleniumWebDriverNunitDemo
+{
+    public class WebDriverTests
+    {
+        private WebDriver driver;
+        private string searchInput;
+
+        [SetUp]
+        public void OpenBrowser()
+        {
+            this.driver = new ChromeDriver();
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+           // this.driver.Quit();
+        }
+
+        [Test]
+        public void Test_CheckTitle()
+        {
+            driver.Url = "https://wikipedia.org";
+            var pageTitle = driver.Title;
+            Assert.That("Wikipedia", Is.EqualTo(pageTitle));
+
+        }
+        [Test]  
+        public void TestWikipediaSearchFiled()
+        {
+            driver.Url = "https://wikipedia.org";
+            var searchfiled = driver.FindElement(By.Id("searchInput"));
+            searchfiled.SendKeys("QA" + Keys.Enter);
+            var pageTitle = driver.Title;
+           Assert.That("QA - Wikipedia", Is.EqualTo(pageTitle));
+           
+        }
+        [Test]
+        public void TestWikipediaCheckItaliano() 
+        {
+            var allStrongElements = driver.FindElement(By.TagName("strong"));
+            var italianoLinkText = allStrongElements[7].Text;
+            Assert.That("Italiano", Is.EqualTo(italianoLinkText));
+
+
+        }
+
+    }
+}
---
+++ SeleniumWebdriver demo/SeleniumWebDriver.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumWebdriver_demo
+{
+    public class SeleniumWebDriver
+    {
+        static void Main(string[] args)
+        {
+            //creating browser
+            var driver = new ChromeDriver();
+            //navi to wikipedia
+            driver.Url = "https://wikipedia.org";
+
+            //get page title
+            var pageTitle = driver.Title;
+            Console.WriteLine("The page title is: " + pageTitle);
+            if (pageTitle == "Wikipedia")
+            {
+                Console.WriteLine("Test PASS");
+
+            }
+            else
+            {
+                Console.WriteLine("Test Fail");
+            }
+            //close browser
+            driver.Quit();  
+            
+        }
+    }
+}
